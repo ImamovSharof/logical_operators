42d35d6c3981fe06f99c9201030243a03496688a
sharof <sharofimamov3@gmail.com>
2022-08-05 11:56:08 +0500

    09

--- bin/logical_operators09.dart
+++ bin/logical_operators09.dart
@@ -6,7 +6,15 @@ Given two integers a, b,  check the following statement "At least one of the num
         a(int): parameter a
         b(int): parameter b
     Returns:
-        bool: answer
+        bool: answer    a%2==0 && b%2==1|| a%2==1 && b%2==0
 */
+bool func(int a, int b){
+    if ( a%2==0 && b%2==1 || a%2==1 && b%2==0 ){
+        return true;
+    }
+    return false;
+}
 
-void main() {}
+void main() {
+    print(func(3,5));
+}
